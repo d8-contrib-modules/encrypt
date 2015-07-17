4f1051babca9a5383e97d1682c5e8a09d311f647
Adam Bergstein <adam.bergstein@acquia.com>
2015-07-16 22:05:11 -0400

    Flushing out the encrypt service based on the settings

--- src/EncryptService.php
+++ src/EncryptService.php
@@ -29,4 +29,49 @@ class EncryptService {
     $this->key_manager = $key_manager;
   }
 
+  /**
+   * Main encrypt function.
+   *
+   * @param string $text
+   *  The plain text to encrypt.
+   *
+   * return string
+   *  The encrypted string.
+   */
+  function encrypt($text) {
+    // Get settings.
+    $settings = \Drupal::config('encrypt.settings');
+
+    // Load the key.
+    $key = $this->key_manager->getKey($settings->get('encryption_key'));
+
+    // Load the encryption method.
+    $enc_method = \Drupal::getContainer()->get('encrypt.encryption')->createInstance($settings->get('encryption_method'));
+
+    // Return the encrypted string.
+    return $enc_method->encrypt($text, $key->getContents());
+  }
+
+  /**
+   * Main decrypt function.
+   *
+   * @param string $text
+   *  The encrypted text to decrypt.
+   *
+   * return string
+   *  The decrypted plain string.
+   */
+  function decrypt($text) {
+    // Get settings.
+    $settings = \Drupal::config('encrypt.settings');
+
+    // Load the key.
+    $key = $this->key_manager->getKey($settings->get('encryption_key'));
+
+    // Load the encryption method.
+    $enc_method = \Drupal::getContainer()->get('encrypt.encryption')->createInstance($settings->get('encryption_method'));
+
+    // Return the encrypted string.
+    return $enc_method->decrypt($text, $key->getContents());
+  }
 }
