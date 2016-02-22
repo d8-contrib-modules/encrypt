25c12506f5548492c8dfc09ed7325089bea642e3
Sven Decabooter <sdecabooter@gmail.com>
2016-02-22 17:04:09 +0100

    Add extra methods to EncryptionProfileManager class

--- src/EncryptionProfileManagerInterface.php
+++ src/EncryptionProfileManagerInterface.php
@@ -13,6 +13,25 @@ namespace Drupal\encrypt;
 interface EncryptionProfileManagerInterface {
 
   /**
+   * Get an encryption profile based on the ID.
+   *
+   * @param string $encryption_profile_id
+   *   ID of EncryptionProfile entity.
+   *
+   * @return \Drupal\encrypt\Entity\EncryptionProfile
+   *   The EncryptionProfile entity.
+   */
+  public function getEncryptionProfile($encryption_profile_id);
+
+  /**
+   * Get all EncryptionProfile entities.
+   *
+   * @return \Drupal\encrypt\Entity\EncryptionProfile[]
+   *   An array of all EncryptionProfile entities.
+   */
+  public function getAllEncryptionProfiles();
+
+  /**
    * Get EncryptionProfile entities by encryption method plugin ID.
    *
    * @param string $encryption_method_id
@@ -33,5 +52,13 @@ interface EncryptionProfileManagerInterface {
    *   An array of EncryptionProfile entities.
    */
   public function getEncryptionProfilesByEncryptionKey($key_id);
+
+  /**
+   * Get EncryptionProfiles as options list for a select element.
+   *
+   * @return array
+   *   An array of encryption profile names, indexed by id.
+   */
+  public function getEncryptionProfileNamesAsOptions();
 
 }
